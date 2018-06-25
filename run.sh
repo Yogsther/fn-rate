70e4d7271e4e113e5fa3c9babec54ca908497cc9
Olle <yogsther@gmail.com>
2018-06-26 00:59:22 +0200

    Added images for gliders and backblings

---
+++ run.sh
@@ -0,0 +1,3 @@
+wget https://image.fnbr.co/backpack/5abcf3839542fb80aeda12ad/icon.png -O img/back_bling/DARK_VOID.png
+
+wget https://image.fnbr.co/backpack/5ace244b1f0d4db6c0d42df3/icon.png -O img/back_bling/FISH_TANK.png
